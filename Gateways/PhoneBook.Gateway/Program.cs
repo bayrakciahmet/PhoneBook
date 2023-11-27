e52387f50911854494183e444530d10d1870419b
Ahmet Bayrakcı <ahmet_bay_42@hotmail.com>
2023-11-27 12:28:38 +0300

    PhoneBook.Gateway Created

---
+++ Gateways/PhoneBook.Gateway/Program.cs
@@ -0,0 +1,16 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile($"configuration.{builder.Environment.EnvironmentName.ToLower()}.json");
+// Add services to the container.
+builder.Services.AddOcelot();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseDeveloperExceptionPage();
+await app.UseOcelot();
+app.Run();
+
+
--- Services/Person/PhoneBook.Services.Person/Program.cs
+++ Services/Person/PhoneBook.Services.Person/Program.cs
@@ -58,6 +58,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.MapControllers();
 app.Run();
 
