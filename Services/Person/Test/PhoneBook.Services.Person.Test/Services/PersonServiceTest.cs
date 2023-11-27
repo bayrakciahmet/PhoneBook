bea1b22bc7cd24f8cda3dbccc0a4cfad1bb5e454
Ahmet BAYRAKCI <bayrakciahmet42@gmail.com>
2023-11-27 11:46:02 +0300

    PhoneBook.Services.Person.Test Created

---
+++ Services/Person/Test/PhoneBook.Services.Person.Test/Services/PersonServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using AutoMapper;
+using MongoDB.Driver;
+using Moq;
+using PhoneBook.Services.Person.Dtos.Persons;
+using PhoneBook.Services.Person.Services.Persons;
+using PhoneBook.Services.Person.Settings.Interfaces;
+using PhoneBook.Shared.Dtos;
+using Xunit;
+namespace PhoneBook.Services.Person.Test.Services
+{
+    public class PersonServiceTests
+    {
+
+    }
+}
